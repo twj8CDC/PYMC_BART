417f2453253cd21e6a57e5dc26edb4b965bbbb59
twj8@cdc.gov <twj8@cdc.gov>
2023-08-07 17:09:14 +0000

    fix value

---
+++ Untitled Notebook 2023-08-07 12_34_03.py
@@ -0,0 +1,10 @@
+# Databricks notebook source
+import mlflow
+
+# COMMAND ----------
+
+mlflow.create_experiment("/Repos/twj8@cdc.gov/PYMC_BART")
+
+# COMMAND ----------
+
+# MAGIC %sh pwd
--- run_start.py
+++ run_start.py
@@ -3,7 +3,7 @@
 # Databricks notebook source
 import mlflow, os
 # proforma, lists modules, maybe sets dictionary of available models as taskValue? 
-experiment_id=dbutils.jobs.taskValues.get("cdh-ml-model","experiment_id")
+experiment_id=dbutils.jobs.taskValues.get("ml-head","experiment_id")
 # model_name=dbutils.widgets.get("model_name")
 model_name = dbutils.jobs.taskValues.get("ml-head", "run_name")
 
@@ -12,6 +12,6 @@ with mlflow.start_run(experiment_id=experiment_id,run_name=f"{model_name}-") as 
    mlflow.log_param('run_id',run_id)
    mlflow.log_param('model_name',model_name)
 
-#os.environ['MLFLOW_RUN_ID']=run_id
-#dbutils.jobs.taskValues.set("run_id",run_id)
-#dbutils.jobs.taskValues.set("experiment_id",experiment_id)
+os.environ['MLFLOW_RUN_ID']=run_id
+dbutils.jobs.taskValues.set("run_id",run_id)
+# dbutils.jobs.taskValues.set("experiment_id",experiment_id)
