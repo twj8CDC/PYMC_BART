26cc936b3920d451eb5278acf0a027c9a208622e
twj8@cdc.gov <twj8@cdc.gov>
2023-08-07 17:42:28 +0000

    fixing exp id

--- run_start.py
+++ run_start.py
@@ -4,11 +4,12 @@
 import mlflow, os
 # proforma, lists modules, maybe sets dictionary of available models as taskValue? 
 experiment_id=dbutils.jobs.taskValues.get("ml-head","experiment_id", debugValue=2256023545555400)
+print(experiment_id)
 
 # model_name=dbutils.widgets.get("model_name")
 model_name = dbutils.jobs.taskValues.get("ml-head", "run_name", debugValue="test2")
 
-with mlflow.start_run(experiment_id=2256023545555400) as run:
+with mlflow.start_run(experiment_id=experiment_id) as run:
    run_id=run.info.run_id
    mlflow.log_param('run_id',run_id)
    mlflow.log_param('model_name',model_name)
