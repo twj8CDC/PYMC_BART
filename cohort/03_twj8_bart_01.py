07d8a0a7dd61d423111106e76d2654b362ea541a
twj8CDC <twj8CDC>
2023-11-30 17:10:08 +0000

    testing the dbutils import

--- cohort/03_twj8_bart_01.py
+++ cohort/03_twj8_bart_01.py
@@ -1,32 +1,32 @@
 # Databricks notebook source
-# MAGIC %pip install scikit-survival pymc pymc_experimental matplotlib colorcet pymc_bart lifelines mlflow
+# %pip install scikit-survival pymc pymc_experimental matplotlib colorcet pymc_bart lifelines mlflow
 
 # COMMAND ----------
 
-import pyspark.sql.functions as F
-import pyspark.sql.window as W
-from pyspark.storagelevel import StorageLevel
+# import pyspark.sql.functions as F
+# import pyspark.sql.window as W
+# from pyspark.storagelevel import StorageLevel
 
 
-import sksurv as sks
-from sksurv import nonparametric
-import matplotlib.pyplot as plt
+# import sksurv as sks
+# from sksurv import nonparametric
+# import matplotlib.pyplot as plt
 import numpy as np
-import pandas as pd
+# import pandas as pd
 
-import sys
-import importlib
-from pathlib import Path
+# import sys
+# import importlib
+# from pathlib import Path
 
-from surv_bart_pkg import surv_bart as bmb
-from surv_bart_pkg import utillities as ut
+# from surv_bart_pkg import surv_bart as bmb
+# from surv_bart_pkg import utillities as ut
 
-import mlflow as ml
-import lifelines as ll
+# import mlflow as ml
+# import lifelines as ll
 import time
 
-importlib.reload(bmb)
-# Set Seed
+# importlib.reload(bmb)
+# # Set Seed
 np_seed = int(np.ceil(time.time()))
 
 # COMMAND ----------
@@ -128,6 +128,11 @@ dbutils.widgets.text("chains", defaultValue=str(CHAINS))
 CHAINS = dbutils.widgets.get("chains")
 CHAINS = int(CHAINS)
 
+
+# COMMAND ----------
+
+import sys
+sys.exit(0)
 
 # COMMAND ----------
 
