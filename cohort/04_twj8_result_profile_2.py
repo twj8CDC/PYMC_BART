ad14a2012d26c196fb60d4e31c9180700f2a4381
twj8CDC <twj8CDC>
2024-01-08 14:59:42 +0000

    clean up push before local pul

--- cohort/04_twj8_result_profile_2.py
+++ cohort/04_twj8_result_profile_2.py
@@ -42,8 +42,13 @@ runs1
 
 # COMMAND ----------
 
-uri = runs1[runs1["tags.mlflow.runName"] == "pcc_BLD_5"]["artifact_uri"].to_numpy()
-runid = runs1[runs1["tags.mlflow.runName"] == "pcc_BLD_5"]["run_id"].to_numpy()
+# MAGIC %md 
+# MAGIC # single result
+
+# COMMAND ----------
+
+uri = runs1[runs1["tags.mlflow.runName"] == "pcc_CIR_5"]["artifact_uri"].to_numpy()
+runid = runs1[runs1["tags.mlflow.runName"] == "pcc_CIR_5"]["run_id"].to_numpy()
 print(uri[0])
 print(runid[0])
 
@@ -54,29 +59,18 @@ client.list_artifacts(runid[0])
 
 # COMMAND ----------
 
-d = np.array(ml.artifacts.load_dict(uri[0] + "/" + "BLD_pdp_trn_prob.json")["data"], dtype="float32")
-
-# COMMAND ----------
-
-d2 = ml.artifacts.load_dict(uri[0] + "/" + "BLD_pdp_trn_prob_dict.json")
-d2
-
-# COMMAND ----------
-
+d = np.array(ml.artifacts.load_dict(uri[0] + "/" + "CIR_pdp_trn_prob.json")["data"], dtype="float32")
+d2 = ml.artifacts.load_dict(uri[0] + "/" + "CIR_pdp_trn_prob_dict.json")
 d = d.reshape(d2["draw"], d2["n"], d2["t"])
 gc.collect()
 
-# COMMAND ----------
-
-# pdp_rr = trn_cov_pdp["pdp_val"]["prob"][:,SAMPLE_TRN:,:]/trn_cov_pdp["pdp_val"]["prob"][:,:SAMPLE_TRN,:]
-
-# COMMAND ----------
-
-d.shape
+# ph
+ph = (d[:,10000:,:]/d[:,:10000,:]).mean(1).mean(1)
 
 # COMMAND ----------
 
-ph = (d[:,5000:,:]/d[:,:5000,:]).mean(1).mean(1)
+d3  = ml.artifacts.load_dict(uri[0] + "/" + "CIR_pdp_rope.json")
+d3
 
 # COMMAND ----------
 
@@ -93,9 +87,85 @@ plt.hist(d[:,:5000,3].mean(1), alpha=0.2, bins = 50)
 # COMMAND ----------
 
 ph_qnt = np.quantile(ph, [0.055,0.945])
-plt.hist(ph, bins = 100)
-plt.axvline(ph_qnt[0])
-plt.axvline(ph_qnt[1])
+plt.hist(ph, bins = 30, color="deepskyblue",
+          weights = np.ones_like(ph) / ph.shape[0])
+# plt.axvline(ph.mean(), color="darkblue", label="mean")
+# plt.axvline(ph_qnt[0], label = "89% low")
+# plt.axvline(ph_qnt[1], label = "89% high")
+plt.axvline(1, color="red", label = "HR=1")
+plt.title("Posterior Distribution HR (CIR)")
+plt.xlabel("HR")
+plt.ylabel("P()")
+plt.legend()
+
+# COMMAND ----------
+
+ph_qnt = np.quantile(ph, [0.055,0.945])
+plt.hist(ph, bins = 30, color="deepskyblue",
+         weights = np.ones_like(ph) / ph.shape[0])
+plt.axvline(ph.mean(), color="darkblue", label="mean")
+plt.axvline(ph_qnt[0], label = "89% low")
+plt.axvline(ph_qnt[1], label = "89% high")
+plt.axvline(1, color="red", label = "HR=1")
+plt.title("Posterior Distribution HR (CIR)")
+plt.xlabel("HR")
+plt.ylabel("P()")
+plt.legend()
+
+# COMMAND ----------
+
+d3
+
+# COMMAND ----------
+
+np.ones_like(ph)
+
+# COMMAND ----------
+
+ph_qnt = np.quantile(ph, [0.055,0.945])
+
+n,bins,patches = plt.hist(ph, bins = 30, color="green",alpha=0.2, 
+                          weights = np.ones_like(ph) / ph.shape[0])
+patch2 = [b for b in bins if b < 1]
+for idx,b in enumerate(bins):
+    if b < 1:
+        patches[idx].set_facecolor("r")
+# text = f"{d3['pd_neg']} %"
+plt.text(s=f'{np.round(d3["pd_neg"],2)} %', x=0.7, y=.1)
+plt.text(s=f'{np.round(d3["pd_pos"],2)} %', x=1.5, y=.1)
+plt.axvline(1, color="red", label = "HR=1")
+plt.title("PD HR (CIR)")
+plt.xlabel("HR")
+plt.ylabel("P()")
+# plt.legend()
+
+# COMMAND ----------
+
+d3
+
+# COMMAND ----------
+
+ph_qnt = np.quantile(ph, [0.055,0.945])
+
+n,bins,patches = plt.hist(ph, bins = 30, color="green",alpha=0.2, 
+                          weights = np.ones_like(ph) / ph.shape[0])
+patch2 = [b for b in bins if b < 1]
+for idx,b in enumerate(bins):
+    if b < 1.01 and b > 0.99:
+        patches[idx].set_facecolor("r")
+# text = f"{d3['pd_neg']} %"
+plt.text(s=f'{np.round(d3["rope_unsig"],2)} %', x=.91, y=.095)
+plt.text(s=f'{np.round(d3["rope_sig"],2)} %', x=1.15, y=.03)
+# plt.axvline(1, color="red", label = "HR=1")
+plt.title("ROPE HR (CIR)")
+plt.xlabel("HR")
+plt.ylabel("P()")
+# plt.legend()
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC # all results
 
 # COMMAND ----------
 
@@ -108,7 +178,9 @@ def get_artifacts(runs):
         uri = r["artifact_uri"]
         # select only json artifacts
         af_list = [x.path for x in client.list_artifacts(run_id = rid) if "json" in x.path]
+        af_list = [x for x in af_list if "pdp_trn_prob" not in x]
         # instantiate dict
+        # print(af_list)
         if idx == 0:
             for af in af_list:
                 d = ml.artifacts.load_dict(uri + "/" + af)
@@ -137,34 +209,38 @@ artf = get_artifacts(runs1)
 
 # COMMAND ----------
 
-p1 = pd.DataFrame(artf["tst_sv_ci_bs"]).T.add_prefix("tst_")
+# p1 = pd.DataFrame(artf["tst_sv_ci_bs"]).T.add_prefix("tst_")
 p2 = pd.DataFrame(artf["trn_sv_ci_bs"]).T.add_prefix("trn_")
 p3 = pd.DataFrame(artf["all_cph_cindex"]).T. add_prefix("cph_all_")
 p4 = pd.DataFrame(artf["trn_cph_cindex"]).T.add_prefix("cph_trn_")
 #
-p5 = pd.DataFrame(artf["tst_sv_calib"]).T.add_prefix("tst_")
+p5 = pd.DataFrame(artf["trn_sv_calib"]).T.add_prefix("trn_")
 p6 = pd.DataFrame(artf["cph_trn_sv_calib"]).T.add_prefix("cph_trn_")
-p7 = pd.DataFrame(artf["cph_tst_sv_calib"]).T.add_prefix("cph_tst_")
+# p7 = pd.DataFrame(artf["cph_tst_sv_calib"]).T.add_prefix("cph_tst_")
 
-metrics = p1.join(p2).join(p3).join(p4).join(p5).join(p6).join(p7)
+# metrics = p1.join(p2).join(p3).join(p4).join(p5).join(p6).join(p7)
+metrics = p2.join(p3).join(p4).join(p5).join(p6)
 
-metrics2 = metrics.drop(["tst_bs", 
-              "trn_bs", 
-              "tst_exp", 
-              "tst_pred", 
-              "tst_qt", 
-              "cph_trn_exp",
-              "cph_trn_pred",
-              "cph_trn_qt",
-              "cph_tst_exp",
-              "cph_tst_pred",
-              "cph_tst_qt",
-              "cph_all_cindex"
-              ], axis=1)
+metrics2 = metrics.drop([
+    # "tst_bs", 
+    "trn_bs", 
+    # "tst_exp", 
+    # "tst_pred", 
+    # "tst_qt", 
+    "cph_trn_exp",
+    "cph_trn_pred",
+    "cph_trn_qt",
+    # "cph_tst_exp",
+    # "cph_tst_pred",
+    # "cph_tst_qt",
+    # "cph_all_cindex"
+    ], 
+    axis=1
+)
 
 # get abs sum of diff
-for i in ["tst_diff", "cph_tst_diff", "cph_trn_diff"]:
-    metrics2[i] = metrics2[i].apply(lambda x: np.abs(x).mean())
+for i in ["trn_diff", "cph_trn_diff"]:
+    metrics2[i] = metrics2[i].apply(lambda x: np.power(x,2).sum())
  
 
 # COMMAND ----------
@@ -174,17 +250,10 @@ metrics2
 
 # COMMAND ----------
 
-import matplotlib.pyplot as plt
-
-# COMMAND ----------
-
-cir_m
-
-# COMMAND ----------
-
 cir_m = metrics2.loc["CIR"]
 plt.plot([1], cir_m["trn_ci"], 'o', label = "BART")
 plt.plot([1], cir_m["cph_trn_cindex"], 'o', label = "CPH")
+plt.plot([1], cir_m["cph_all_cindex"], "o", label = "CPH_ALL")
 plt.legend()
 
 
@@ -192,9 +261,10 @@ plt.legend()
 
 # plot
 fig,ax = plt.subplots(1, figsize=(10,10))
-ax.plot(metrics2.index, metrics2.tst_ci, label = "tst_bart")
+# ax.plot(metrics2.index, metrics2.tst_ci, label = "tst_bart")
 ax.plot(metrics2.index, metrics2.trn_ci , label = "trn_bart")
 ax.plot(metrics2.index, metrics2.cph_trn_cindex, label = "trn_cph")
+ax.plot(metrics2.index, metrics2.cph_all_cindex, label = "trn_cph")
 ax.set_xticklabels(metrics2.index, rotation=90)
 ax.set_title("C-Index")
 ax.legend()
@@ -202,22 +272,22 @@ ax.legend()
 
 # COMMAND ----------
 
-fig,ax = plt.subplots(1, figsize=(10,10))
-ax.plot(metrics2.index, metrics2.tst_ibs, label = "tst_bart")
-ax.plot(metrics2.index, metrics2.trn_ibs , label = "trn_bart")
-ax.set_xticklabels(metrics2.index, rotation=90)
-ax.set_title("Integrated Brier Score")
-ax.legend()
+# fig,ax = plt.subplots(1, figsize=(10,10))
+# ax.plot(metrics2.index, metrics2.tst_ibs, label = "tst_bart")
+# ax.plot(metrics2.index, metrics2.trn_ibs , label = "trn_bart")
+# ax.set_xticklabels(metrics2.index, rotation=90)
+# ax.set_title("Integrated Brier Score")
+# ax.legend()
 
 # COMMAND ----------
 
 fig,ax = plt.subplots(1, figsize=(10,10))
-ax.plot(metrics2.index, metrics2.tst_diff, label = "tst_bart")
+ax.plot(metrics2.index, metrics2.trn_diff, label = "tst_bart")
 ax.plot(metrics2.index, metrics2.cph_trn_diff, label = "trn_cph")
-ax.plot(metrics2.index, metrics2.cph_tst_diff, label = "tst_cph")
+# ax.plot(metrics2.index, metrics2.cph_tst_diff, label = "tst_cph")
 ax.set_xticklabels(metrics2.index, rotation=90)
 ax.set_title("Calibration Difference (by last time)")
-ax.set_ylabel("Mean Absolute Error")
+ax.set_ylabel("Sum of Squared Error")
 ax.legend()
 
 # COMMAND ----------
@@ -236,13 +306,25 @@ ax.legend()
 
 # COMMAND ----------
 
-p1 =pd.DataFrame(artf["pdp_covid_trn_tst"]).T.add_prefix("bart_")
+rope = pd.DataFrame(artf["pdp_rope"]).T
+rope
+
+# COMMAND ----------
+
+p1 =pd.DataFrame(artf["pdp_covid_trn"]).T.add_prefix("bart_")
 covid_all = {k:v["covid_icd_lab"] for k,v in artf["all_cph_result"].items()}
 covid_trn = {k:v["covid_icd_lab"] for k,v in artf["trn_cph_result"].items()}
 p2 = pd.DataFrame(covid_all).T.add_prefix("cph_all_")
 p3 = pd.DataFrame(covid_trn).T.add_prefix("cph_trn_")
+rope = pd.DataFrame(artf["pdp_rope"]).T.add_prefix("bart_")
 
-res = p1.join(p2).join(p3)
+res = p1.join(p2).join(p3).join(rope)
+
+# COMMAND ----------
+
+# get the other metrics
+
+res
 
 # COMMAND ----------
 
@@ -250,9 +332,19 @@ res = p1.join(p2).join(p3)
 
 # COMMAND ----------
 
+res["bart_pd_max"] = res[["bart_pd_pos","bart_pd_neg"]].max(axis=1)
+
+# COMMAND ----------
+
+# def rope_sig(x):
+#     if x < 2.5:
+#         out = "sig"
+
+# COMMAND ----------
+
 res_rr = res[[
     "bart_trn_rr",
-    "bart_tst_rr",
+    # "bart_tst_rr",
     "cph_trn_exp(coef)",
     "cph_trn_exp(coef) lower 95%",
     "cph_trn_exp(coef) upper 95%",
@@ -260,7 +352,10 @@ res_rr = res[[
     "cph_all_exp(coef) lower 95%",
     "cph_all_exp(coef) upper 95%",
     "cph_trn_p",
-    "cph_all_p"
+    "cph_all_p",
+    "bart_pd_max",
+    "bart_rope_sig",
+    "bart_rope_unsig"
 ]]
 res_rr["bart_rr_ml"] = res_rr["bart_trn_rr"].apply(lambda x: x["rr_m"])
 res_rr["bart_rr_ll"] = res_rr["bart_trn_rr"].apply(lambda x: x["rr_q"][0])
@@ -271,12 +366,24 @@ res_rr["bart_rr_l"] = res_rr["bart_trn_rr"].apply(lambda x: np.mean(x["rr_q"][0]
 res_rr["bart_rr_u"] = res_rr["bart_trn_rr"].apply(lambda x: np.mean(x["rr_q"][1]))
 res_rr["cph_trn_sig"] = res_rr["cph_trn_p"].apply(lambda x: 3 if x < 0.05 else np.NAN)
 res_rr["cph_all_sig"] = res_rr["cph_all_p"].apply(lambda x: 2.9 if x < 0.05 else np.NAN)
+res_rr["bart_pd_sig"] = res_rr["bart_pd_max"].apply(lambda x: 3.3 if x > 0.95 else np.NAN) 
+res_rr["bart_rope_issig"] = res_rr["bart_rope_unsig"].apply(lambda x: 3.5 if x < 0.025 else np.NAN)
+res_rr["bart_rope_isund"] = res_rr["bart_rope_unsig"].apply(lambda x: 3.5 if (x >=0.025 and x <= 0.975) else np.NAN)
+res_rr["bart_rope_isunsig"] = res_rr["bart_rope_unsig"].apply(lambda x: 3.5 if x >= 0.975 else np.NAN)
+
+
+res_rr = res_rr.drop(index="PNL", axis=1)
 
 
 # COMMAND ----------
 
+res_rr
+
+# COMMAND ----------
+
 cir_hr = pd.DataFrame(res_rr.loc["CIR"]).T
-cir_hr
+cir_hr["cph_trn_sig"] = 1.79
+cir_hr["cph_all_sig"] = 1.8
 
 # COMMAND ----------
 
@@ -294,7 +401,8 @@ def plot_rr(res_rr):
                     res_rr.bart_rr_u - res_rr.bart_rr_m
                 ),
                 fmt = "o",
-                label = "bart"
+                label = "bart",
+                color = "deepskyblue"
                 )
     ax.errorbar(y = inds + 0.02, 
                 x = res_rr["cph_all_exp(coef)"],
@@ -303,7 +411,8 @@ def plot_rr(res_rr):
                     res_rr["cph_all_exp(coef) upper 95%"] - res_rr["cph_all_exp(coef)"], 
                 ),
                 fmt = "o",
-                label = "cph_all"
+                label = "cph_all",
+                color = "orchid"
                 )
     ax.errorbar(y =inds + .01, 
                 x = res_rr["cph_trn_exp(coef)"],
@@ -313,10 +422,11 @@ def plot_rr(res_rr):
                 ),
                 alpha=0.5,
                 fmt = "o",
-                label = "cph_trn"
+                label = "cph_trn",
+                color = "darkorange"
                 )
-    ax.plot(res_rr["cph_trn_sig"], inds+.01, "*", color = "green")
-    ax.plot(res_rr["cph_all_sig"], inds+.02, "*", color = "darkorange")
+    ax.plot(res_rr["cph_trn_sig"], inds+.01, "*", color = "darkorange")
+    ax.plot(res_rr["cph_all_sig"], inds+.02, "*", color = "orchid")
     ax.set_yticks(inds)
     ax.set_yticklabels("")
     # ax.plot(np.repeat(1,len(inds)), inds, "--")
@@ -334,9 +444,9 @@ res_rr.columns
 # COMMAND ----------
 
 ind = res_rr.index.to_list()
-print(ind)
+# print(ind)
 inds = np.arange(1,len(ind)+1)
-print(inds)
+# print(inds)
 fig,ax = plt.subplots(1, figsize=(10,10))
 # ax.plot(ind, res_rr.bart_rr_m)
 ax.errorbar(y = inds, 
@@ -346,7 +456,8 @@ ax.errorbar(y = inds,
                 res_rr.bart_rr_u - res_rr.bart_rr_m
             ),
             fmt = "o",
-            label = "bart"
+            label = "bart",
+            color = "deepskyblue"
             )
 ax.errorbar(y = inds - .2, 
             x = res_rr["cph_all_exp(coef)"],
@@ -355,7 +466,8 @@ ax.errorbar(y = inds - .2,
                 res_rr["cph_all_exp(coef) upper 95%"] - res_rr["cph_all_exp(coef)"], 
             ),
             fmt = "o",
-            label = "cph_all"
+            label = "cph_all",
+            color = "orchid"
             )
 ax.errorbar(y =inds + .2, 
             x = res_rr["cph_trn_exp(coef)"],
@@ -365,11 +477,16 @@ ax.errorbar(y =inds + .2,
             ),
             fmt = "o",
             label = "cph_trn",
-            color = "green",
-            alpha=0.5
+            color = "darkorange",
+            # alpha=0.5
             )
-ax.plot(res_rr["cph_trn_sig"], inds+.1, "*", color = "green")
-ax.plot(res_rr["cph_all_sig"], inds+.1, "*", color = "darkorange")
+ax.plot(res_rr["cph_trn_sig"], inds+.1, "*", color = "darkorange")
+ax.plot(res_rr["cph_all_sig"], inds+.1, "*", color = "orchid")
+# ax.plot(res_rr["bart_rope_issig"], inds, marker="P", markersize=10, color = "green", label="rope_unsig")
+# ax.plot(res_rr["bart_rope_isund"], inds, marker="P", markersize=10, color = "khaki", label="rope_sig")
+# ax.plot(res_rr["bart_rope_isunsig"], inds, marker="P", markersize=10, color = "red")
+# ax.plot(res_rr["bart_pd_sig"], inds, marker=">", markersize=10, color = "darkseagreen", label="pd_sig")
+# ax.plot(res_rr["bart_pd_sig"], inds, marker=">", markersize=10, color = "d")
 ax.set_yticks(inds)
 ax.set_yticklabels(ind)
 # ax.plot(np.repeat(1,len(inds)), inds, "--")
@@ -377,6 +494,7 @@ ax.axvline(1, linestyle="--", color="red")
 ax.legend()
 ax.set_title("Hazard Ratios COVID Condition")
 ax.set_xlabel("HR")
+ax.grid(alpha=0.2)
 
 # ax.set_xlim((, 2))
 
@@ -395,18 +513,18 @@ ax.set_xlabel("HR")
 
 def get_diff(artf, time):
     t = time-1
-    p1 = pd.DataFrame(artf["pdp_covid_trn_tst"]).T
+    p1 = pd.DataFrame(artf["pdp_covid_trn"]).T
     p1["trn_m"] = p1["trn_diff"].apply(lambda x: x["diff_m"][t] * -1)
     p1["trn_l"] = p1["trn_diff"].apply(lambda x: x["diff_q"][0][t] * -1)
     p1["trn_h"] = p1["trn_diff"].apply(lambda x: x["diff_q"][1][t] * -1)
-    p1["tst_m"] = p1["tst_diff"].apply(lambda x: x["diff_m"][t] * -1)
-    p1["tst_l"] = p1["tst_diff"].apply(lambda x: x["diff_q"][0][t] * -1)
-    p1["tst_h"] = p1["tst_diff"].apply(lambda x: x["diff_q"][1][t] * -1)
+    # p1["tst_m"] = p1["tst_diff"].apply(lambda x: x["diff_m"][t] * -1)
+    # p1["tst_l"] = p1["tst_diff"].apply(lambda x: x["diff_q"][0][t] * -1)
+    # p1["tst_h"] = p1["tst_diff"].apply(lambda x: x["diff_q"][1][t] * -1)
     return p1
 
 def get_diff_mean_time(artf, time):
     t = time -1
-    p2 = pd.DataFrame(artf["tst_sv_cov_ncov_mq"]).T
+    p2 = pd.DataFrame(artf["trn_sv_cov_ncov_mq"]).T
     p3 =  np.round(np.hstack([
         np.array([x[t] for x in p2.mt_m.to_numpy()]).reshape(-1,1),
         np.array([x[t] for x in p2.mf_m.to_numpy()]).reshape(-1,1)
@@ -430,15 +548,15 @@ def plt_diff(p1, p3, time):
                 fmt = "o",
                 label = "bart_trn"
                 )
-    ax.errorbar(y = inds + .2, 
-                x = p1.tst_m,
-                xerr = (
-                    p1.tst_m - p1.tst_l,
-                    p1.tst_h - p1.tst_m
-                ),
-                fmt = "o",
-                label = "bart_tst"
-                )
+    # ax.errorbar(y = inds + .2, 
+    #             x = p1.trn_m,
+    #             xerr = (
+    #                 p1.trn_m - p1.trn_l,
+    #                 p1.trn_h - p1.trn_m
+    #             ),
+    #             fmt = "o",
+    #             label = "bart_tst"
+    #             )
     # ax.plot(res_rr["cph_trn_sig"], inds+.1, "*", color = "green")
     # ax.plot(res_rr["cph_all_sig"], inds+.1, "*", color = "darkorange")
     ax.set_yticks(inds)
@@ -451,7 +569,7 @@ def plt_diff(p1, p3, time):
     ax.set_title(f"Marginal Distribution COVID Condition ({t} days)")
     ax.set_xlabel("Survival Difference (Covid - Non-Covid)")
     for idx, i in enumerate(p3):
-        ax.text(y = inds[idx], x = -0.075, s=i)
+        ax.text(y = inds[idx], x = -0.065, s=i)
     return fig
 
 
